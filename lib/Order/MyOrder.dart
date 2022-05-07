eb665c6c6b2e5b303aa947cc5a2d2095ec125b05
Prabesh Rai <raiprabesh789@gmail.com>
2022-05-07 22:46:43 +0545

    fix: my order issuse fiexd.

--- lib/Order/MyOrder.dart
+++ lib/Order/MyOrder.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:ecommerce_app/Services/Orderservices.dart';
 import 'package:ecommerce_app/Utils/Colors.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -25,95 +24,102 @@ class MyOrder extends StatelessWidget {
         centerTitle: true,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              child: StreamBuilder<QuerySnapshot>(
-                  stream: _order.orders
-                      .where('userId', isEqualTo: user!.uid)
-                      .snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    print(user.uid);
-                    if (snapshot.hasError) {
-                      return Text('Something went Wrong');
-                    }
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(child: CircularProgressIndicator());
-                    }
-                    if (!snapshot.hasData) {
-                      return Center(child: Text('No Orders, Continue'));
-                    }
-
-                    return Expanded(
-                      child: ListView(
-                          shrinkWrap: true,
-                          children: snapshot.data!.docs
-                              .map((DocumentSnapshot document) {
-                            return Container(
-                              color: Colors.white,
-                              child: Column(children: [
-                                ListTile(
-                                  leading: CircleAvatar(
-                                      backgroundColor: AppColors.buttonColor,
-                                      radius: 14,
-                                      child: Icon(Icons.list)),
-                                  title: Text(
-                                    document['orderStatus'],
-                                  ),
-                                  trailing: Text(
-                                      'Amount: Rs.${document['total'].toStringAsFixed(0)}'),
-                                ),
-                                ExpansionTile(
-                                    title: Text('Order Details',
-                                        style: TextStyle(
-                                            fontSize: 12, color: Colors.black)),
-                                    subtitle: Text('View Other details',
-                                        style: TextStyle(
-                                            fontSize: 12, color: Colors.black)),
+        child: StreamBuilder(
+          stream:
+              _order.orders.where('userId', isEqualTo: user?.uid).snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went Wrong');
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (!snapshot.hasData) {
+              return Center(child: Text('No Orders, Continue'));
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    final document = snapshot.data?.docs[index];
+                    final productDoc = snapshot.data?.docs;
+                    return Container(
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          ListTile(
+                            leading: CircleAvatar(
+                                backgroundColor: AppColors.buttonColor,
+                                radius: 14,
+                                child: Icon(Icons.list)),
+                            title: Text(
+                              document?['orderStatus'],
+                            ),
+                          ),
+                          ExpansionTile(
+                            title: Text(
+                              'Order Details',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.black),
+                            ),
+                            subtitle: Text(
+                              'View Other details',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.black),
+                            ),
+                            children: [
+                              ListView.builder(
+                                itemCount:
+                                    productDoc?[index]['products'].length,
+                                shrinkWrap: true,
+                                itemBuilder: (BuildContext context, int item) {
+                                  return Column(
                                     children: [
-                                      ListView.builder(
-                                        shrinkWrap: true,
-                                        itemBuilder:
-                                            (BuildContext context, int index) {
-                                          return ListTile(
-                                            leading: CircleAvatar(
-                                                child: Image.network(
-                                                    document['products'][index]
-                                                        ['img'])),
-                                            title: Text(document['products']
-                                                [index]['productName']),
-                                          );
-                                        },
-                                        itemCount: document['products'].length,
+                                      ListTile(
+                                        leading: CircleAvatar(
+                                          child: Image.network(
+                                            productDoc?[index]['products'][item]
+                                                ['img'],
+                                          ),
+                                        ),
+                                        title: Text(productDoc?[index]
+                                            ['products'][item]['productName']),
                                       ),
                                       Padding(
-                                          padding: EdgeInsets.only(
-                                              left: 12,
-                                              right: 12,
-                                              top: 8,
-                                              bottom: 8),
-                                          child: Card(
-                                              color: const Color(0xFFe2efdd),
-                                              child: Padding(
-                                                  padding: EdgeInsets.all(8.0),
-                                                  child: Column(children: [
-                                                    Row(
-                                                      children: [
-                                                        Text('Seller :'),
-                                                        Text(document[
-                                                            'storeName']),
-                                                      ],
-                                                    )
-                                                  ]))))
-                                    ]),
-                              ]),
-                            );
-                          }).toList()),
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 12, vertical: 8),
+                                        child: Card(
+                                          color: const Color(0xFFe2efdd),
+                                          child: Padding(
+                                            padding: EdgeInsets.all(8.0),
+                                            child: Column(
+                                              children: [
+                                                Row(
+                                                  children: [
+                                                    Text('Seller :'),
+                                                    Text(
+                                                      productDoc?[index]
+                                                              ['products'][item]
+                                                          ['storeName'],
+                                                    ),
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                    ],
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     );
-                  }),
-            ),
-          ],
+                  });
+            }
+          },
         ),
       ),
     );
